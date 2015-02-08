b16b33be8ea7e3672775d2250c1decf380ce7e7a
Maarten <lucmaar@hotmail.com>
2015-02-09 00:22:00 +0100

    Ljud borde funkar nu.

--- spelmotorn/Sound.java
+++ spelmotorn/Sound.java
@@ -1,71 +1,49 @@
-/*package spelmotorn;
+package spelmotorn;
 
-import java.applet.AudioClip;
+import java.applet.*;
 import java.io.*;
 import sun.audio.*;
-import javax.sound.*;
 
-public class Sound {
+public class Sound{
 	
 	AudioPlayer soundPlayer = AudioPlayer.player;
 	AudioStream backgroundMusic;
-	AudioData backgroundData;	
-	ContinousAudioDataStream backgroundLoop = null;
+	AudioStream soundFile;
+	AudioData backgroundData;
+	AudioData soundData;
+	ContinuousAudioDataStream backgroundLoop = null;
 	
 	AudioClip actionSound;
+	AudioClip jumpSound;
 
- * Lägg till ljud för varje action som behöver ljud.
- * ex: punchSound -> getAudioClip
- * 
- */
-/*	AudioClip jumpSound;
+	String filePath = "";
 	
-	public void loadSounds(){
-		backgroundSound();
+	Sound(String filePath){
+		this.filePath = filePath;
 	}
 	
-	protected void backgroundSound(){
-		
-		try{
-/*
- * Välj fil för bakgrunds musiken -> continious loop
- */
-/*			backgroundMusic = new AudioStream(new FileInputStream(""));
-			backgroundData = backgroundMusic.getData();
-			backgroundLoop = new ContiniousAudioDataStream(backgroundData);
-		}catch(IOException e){
-			System.out.println("Fel på backgroundsmusic");
-		}
-		soundPlayer.start(backgroundLoop); 
-	}
-	
-	protected void actionSoundMethod(){
-/*
- * Välj namn på filen -> Action -> Keypressed/Movement sound
- * För varje action -> en metod -> för varje keypressed -> ljud om det krävs.
- * Skapa ny metod (annan namn) med samma try/catch -> Välj ny fil -> ny AudioClip 
- */
-/*		try{
-		actionSound = getAudioClip(getCodeBase(), "");
-		
-		}catch (IOException e){
-			System.out.println("Fel på action sound filen");
-		}
-		actionSound.play();
-	}
-	
-/*
- * Exemple ljud metod.
- * Hopp ljud. jumpSoundMethod() i input för keypressed "jump".
- */
-/*	protected void jumpSoundMethod(){
-		try{
-		jumpSound = getAudioClip(getCodeBase(), "");
-		
-		}catch (IOException e){
-			System.out.println("Fel på action sound filen");
-		}
-		jumpSound.play();
-	}
-	
-}*/
+ 	public String getPath(){
+ 		return this.filePath;
+ 	}
+ 	
+ 	public void playSound(){
+ 		try{
+ 			soundFile = new AudioStream(new FileInputStream(getPath()));
+ 			soundData = soundFile.getData();
+ 		}catch (IOException e){
+ 			System.out.println("Fel på filen");
+ 		}
+ 		soundPlayer.start(soundFile);
+ 	}
+ 	
+ 	public void playSoundBackground(){
+ 		try{
+ 			backgroundMusic = new AudioStream(new FileInputStream(getPath()));
+ 			backgroundData = backgroundMusic.getData();
+ 			backgroundLoop = new ContinuousAudioDataStream(backgroundData);
+ 		}catch(IOException e){
+ 			soundPlayer.start(backgroundLoop);
+ 		}
+ 	}
+
+}
